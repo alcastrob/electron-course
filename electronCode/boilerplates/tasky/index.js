34ea941ab326463c3de840c0a2a47a060ba831c1
Angel Castro <al.castro@gmail.com>
2018-12-02 14:45:27 +0100

    Hidding the app icon when showed.

--- electronCode/boilerplates/tasky/index.js
+++ electronCode/boilerplates/tasky/index.js
@@ -1,11 +1,10 @@
 const electron = require('electron');
 const path = require('path');
-const TimerTray = require ('./app/timer_tray');
+const TimerTray = require('./app/timer_tray');
 
 const {
     app,
-    BrowserWindow,
-    Tray
+    BrowserWindow
 } = electron;
 
 let mainWindow;
@@ -18,8 +17,16 @@ app.on('ready', () => {
         resizable: false,
         show: false
     });
+    if (process.platform === 'darwing') {
+        app.dock.hide();
+    } else {
+        mainWindow.setSkipTaskbar(true);
+    }
     mainWindow.loadURL(`file://${__dirname}/src/index.html`);
-    
+    mainWindow.on('blur', () => {
+        mainWindow.hide();
+    });
+
     const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
     const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
     //tray = new Tray(iconPath);
