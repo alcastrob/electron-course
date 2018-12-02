a0666f4b0ee94ff483b257bef4c9b40c32aa4488
Angel Castro <al.castro@gmail.com>
2018-12-02 13:26:14 +0100

    Tray icon and applications shows and hide in the proper position.

--- electronCode/boilerplates/tasky/index.js
+++ electronCode/boilerplates/tasky/index.js
@@ -1,17 +1,46 @@
 const electron = require('electron');
+const path = require('path');
+
 const {
     app,
-    BrowserWindow
+    BrowserWindow,
+    Tray
 } = electron;
 
 let mainWindow;
+let tray;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
         height: 500,
         width: 300,
         frame: false,
-        resizable: false
+        resizable: false,
+        show: false
     });
     mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+    
+    const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
+    const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+    tray = new Tray(iconPath);
+    tray.on('click', (event, bounds) => {
+        //Click event bounds
+        const { x, y } = bounds;
+        //Window height and width
+        const { height, width } = mainWindow.getBounds();
+
+
+        if (mainWindow.isVisible()) {
+            mainWindow.hide();
+        } else {
+            const yPosition = process.platform === 'darwin' ? y : y - height;
+            mainWindow.setBounds({
+                x: x - width / 2,
+                y: yPosition,
+                height: height,
+                width: width
+            });
+            mainWindow.show();
+        }
+    });
 });
