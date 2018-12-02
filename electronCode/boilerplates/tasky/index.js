72fd424ac71daa15547ed37cd05fca3ee7ff59e8
Angel Castro <al.castro@gmail.com>
2018-12-02 14:12:19 +0100

    Refactor of the main js. Created a TimerTray subclass.

--- electronCode/boilerplates/tasky/index.js
+++ electronCode/boilerplates/tasky/index.js
@@ -1,5 +1,6 @@
 const electron = require('electron');
 const path = require('path');
+const TimerTray = require ('./app/timer_tray');
 
 const {
     app,
@@ -8,7 +9,6 @@ const {
 } = electron;
 
 let mainWindow;
-let tray;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
@@ -22,25 +22,6 @@ app.on('ready', () => {
     
     const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
     const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
-    tray = new Tray(iconPath);
-    tray.on('click', (event, bounds) => {
-        //Click event bounds
-        const { x, y } = bounds;
-        //Window height and width
-        const { height, width } = mainWindow.getBounds();
-
-
-        if (mainWindow.isVisible()) {
-            mainWindow.hide();
-        } else {
-            const yPosition = process.platform === 'darwin' ? y : y - height;
-            mainWindow.setBounds({
-                x: x - width / 2,
-                y: yPosition,
-                height: height,
-                width: width
-            });
-            mainWindow.show();
-        }
-    });
+    //tray = new Tray(iconPath);
+    new TimerTray(iconPath, mainWindow);
 });
